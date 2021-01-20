517650b9fc388ebe16b112655ea02fd8b4340673
Abhimanyu Saxena <71390499+AbhimanyuSaxenaSol@users.noreply.github.com>
2021-01-20 21:14:15 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from flask import Flask, jsonify, request, abort
 
 
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
 
 
 def _logger():
@@ -79,7 +79,7 @@ def auth():
 
     user_data = body
 
-    return jsonify(token=_get_jwt(user_data).decode('utf-8'))
+    return jsonify(token=_get_jwt(user_data))
 
 
 @APP.route('/contents', methods=['GET'])
